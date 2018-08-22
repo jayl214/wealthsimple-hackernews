6945196804f0770dd3822c8e752eea860fd721a1
jayl214 <jay.lin@mail.mcgill.ca>
2018-08-22 04:22:07 -0400

    responsive navbar overlay

---
+++ public/scripts/responsiveNavBar.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+  document.getElementById("responsiveNavBar").style.width = "100vw";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("responsiveNavBar").style.width = "0";
+}
